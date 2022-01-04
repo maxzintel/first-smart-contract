1cb31cd00c7cdfc6013c66599c819ef3cd3ac79f
s1dequest <maxzintel@gmail.com>
2022-01-03 22:09:18 -0500

    Syntax fixes per updated version.

--- contracts/1_message.sol
+++ contracts/1_message.sol
@@ -1,18 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.4.17;
+pragma solidity >0.4.17;
 
 contract Inbox {
     string public message; // string type, public = accessible to everyone in the world.
 
-    function Inbox(string initialMessage) public {
+    // Explicit data location for all variables of struct, array or mapping types is now mandatory, hence adding the memory bit. This now stores the parameter in local system memory.
+    // External Functions require parameters with data location of calldata.
+    function inbox(string memory initialMessage) public {
         message = initialMessage;
     }
 
-    function setMessage(string newMessage) public {
+    function setMessage(string memory newMessage) public {
         message = newMessage;
     }
 
-    function getMessage() public view returns (string) {
+    function getMessage() public view returns (string memory) {
         return message;
     }
 }
