fb55b85f63d67af60f9016ed7b2c598198d414cb
s1dequest <maxzintel@gmail.com>
2022-05-01 13:30:06 -0400

    Function to do math and example gas costs for each workload.

--- contracts/1_message.sol
+++ contracts/1_message.sol
@@ -1,36 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0
 
-// specifies the version of solidity that our code is written in.
 pragma solidity >0.4.17;
 
-// Defines a new contract (class) that will have some number of methods and vars.
-// Contract == Class in JS. Thus multiple instances of the contract may be deployed at any given time.
 contract Inbox {
 
-    // Declares all of the instance variables and their types that will exist in the contract.
-    // Instance variables are around for the life of the contract.
-    // String == Type.
-    // This is a Storage Variable == A var that will be automatically stored with the contract on the blockchain. For all eternity!
-    string public message; // string type, public = accessible to everyone in the world.
-    // ^ public function is automatically created. Making getMessage superfluous.
+    string public message;
 
-    // Below are the functions that will be members of the contract.
-
-    // A constructor is just a function that is automatically run once when the contract is created.
     constructor(string memory initialMessage) {
         message = initialMessage;
     }
 
-    // Normal functions are functions we can optionally call after the contract has been deployed to the blockchain.
-
-    //                can be called with this arg.
     function setMessage(string memory newMessage) public {
-        message = newMessage; // accesses the argument with this var.
+        message = newMessage;
     }
 
-    // Removed, see comment on var.
-    //       fnc name     fnc type    return types
-    // function getMessage() public view returns (string memory) {
-    //     return message;
-    // }
+    function doMath(int a, int b) public {
+      a + b;
+      b - a;
+      a * b;
+      a == 0;
+    }
 }
