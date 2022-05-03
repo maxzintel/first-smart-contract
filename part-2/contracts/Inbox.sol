674ac528fe08f90733309c055719edee05db7112
s1dequest <maxzintel@gmail.com>
2022-05-03 10:46:34 -0400

    Move inbox code to new file for part 2 of course.

--- part-2/contracts/Inbox.sol
+++ part-2/contracts/Inbox.sol
@@ -2,3 +2,16 @@ pragma solidity ^0.4.17;
 // linter warnings (red underline) about pragma version can igonored!
 
 // contract code will go here
+// SPDX-License-Identifier: GPL-3.0
+contract Inbox {
+
+    string public message;
+
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
