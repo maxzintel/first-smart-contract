fd818423cd1c4ae1bfd9d7b5381ab2f42d538465
s1dequest <maxzintel@gmail.com>
2022-05-10 09:41:26 -0400

    Import libraries to test file

--- part-2/test/Inbox.test.js
+++ part-2/test/Inbox.test.js
@@ -1,3 +1,6 @@
 // contract test code will go here
 // GOAL: Make sure the contracts are working in the way we designed them.
 
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
