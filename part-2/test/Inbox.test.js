54a629eb8bb382cc61c235cf150ddc7ddd414a85
s1dequest <maxzintel@gmail.com>
2022-05-31 10:00:26 -0400

    Create local test net with test eth addresses.

--- part-2/test/Inbox.test.js
+++ part-2/test/Inbox.test.js
@@ -5,3 +5,17 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); // Capital W means we are the constructor here.
 const web3 = new Web3(ganache.provider()); // instantiating the constructor above. attempts to connect to local test network. Different providers will be set for 'production' networks.
+
+beforeEach(() => {
+  // Get a list of all accounts
+  web3.eth.getAccounts()
+    .then(fetchedAccounts => {
+      console.log(fetchedAccounts);
+    });
+
+  // Use one of those accounts to deploy the contract
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {});
+})
