3e5a3ba8f174448b5934c5e19b5c36fa718e8b37
s1dequest <maxzintel@gmail.com>
2022-06-01 10:12:03 -0400

    Couple relevant tests and explanatory comments.

--- part-2/test/Inbox.test.js
+++ part-2/test/Inbox.test.js
@@ -9,19 +9,33 @@ const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const defaultMessage = 'Hi there!';
 
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts()
 
   // Use one of those accounts to deploy the contract
+  // line 1 below teaches web3 about what methods an Inbox contract has.
+  // line 2 tells web3 that we want to deploy a new copy of this contract.
+    // arguments = list of args to pass into the constructor function when the contract is created.
+  // line 3 tells web3 to send out a transaction that creates the contract.
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
-    .send({ from: accounts[0], gas: '1000000' })
+    .deploy({ data: bytecode, arguments: [defaultMessage] })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(inbox);
+    // assert the value is truthy, not null or undefined, and is a string
+    assert.ok(inbox.options.address); // when you console log inbox, options and address are a part of the object. This outputs the address the contract was deployed to.
+  });
+
+  // calling a method here, is relatively instant.
+  it('has a default message', async () => {
+    // in messages (), pass in any required args if any.
+    // in call (), 
+    const message = await inbox.methods.message().call();
+    assert.equal(message, defaultMessage);
   });
 })
